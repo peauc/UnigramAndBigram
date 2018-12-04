8446f22f5dfa3886caf2d41fb9e7c7dafdb764ad
PEAU Clement <clement.peau@epitech.eu>
2018-12-04 14:08:02 -0500

    Getting the probabilities straight

--- src/NGram.py
+++ src/NGram.py
@@ -1,21 +1,33 @@
 import string
 from src.Utils import Utils
 
+
 class NGram:
-    def __init__(self, size, character_set):
+    def __init__(self, size, character_set, delta):
         self.__size = size
-        self.__gram = Utils.make_empty_gram(self.size, character_set)
+        self.__total_number_of_grams = 0
+        self.__gram = self.__make_empty_gram(self.size, character_set, delta)
         self.__language = ""
 
-    #region Properties
+#region Properties
     @property
     def language(self):
         return self.__language
 
     @property
+    def total_number_of_grams(self):
+        return self.__total_number_of_grams
+
+    @total_number_of_grams.setter
+    def total_number_of_grams(self, value):
+        self.__total_number_of_grams = value
+
+    @property
     def size(self):
         return self.__size
-    #endregion
+#endregion
+
+
 
     # TODO: Didn't find a way to pass the formating method to the function without rewriting one myself
     #  and i'm lazy so hardcoding for now
@@ -26,16 +38,27 @@ class NGram:
         for g in grams:
             self._feed_grams(self.__gram, g)
 
-    def predict(self, character):
-
-        return character, 0
+    def predict(self, ngram):
+        return ngram, 0
 
     def _feed_grams(self, gram, g):
         if len(g) == 1:
+            self.__total_number_of_grams += 1
             gram[g[0]] += 1
             return
         self._feed_grams(gram[g[0]], g[1:])
 
+    def __make_empty_gram(self, size, character_set, delta):
+        ret = dict()
+
+        if size == 1:
+            for idx, c in enumerate(character_set):
+                ret[c] = delta
+                self.__total_number_of_grams += delta
+            return ret
+        for idx, c in enumerate(character_set):
+            ret[c] = self.__make_empty_gram(size - 1, character_set, delta)
+        return ret
 
 
 
--- src/Utils.py
+++ src/Utils.py
@@ -1,16 +1,4 @@
 class Utils:
     @staticmethod
-    def make_empty_gram(size, character_set):
-        ret = dict()
-
-        if size == 1:
-            for idx, c in enumerate(character_set):
-                ret[c] = 0
-            return ret
-        for idx, c in enumerate(character_set):
-            ret[c] = Utils.make_empty_gram(size - 1, character_set)
-        return ret
-
-    @staticmethod
     def format_gram_to_joint_probability(param):
         return param
